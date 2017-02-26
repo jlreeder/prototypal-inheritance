cd4a84492738d625436653a08bc005f842a3acd2
Jason Reeder <info@jasonreeder.com>
2017-02-26 12:14:20 -0800

    Define function after inheritance
    The code breaks without this

--- inheritance.js
+++ inheritance.js
@@ -4,6 +4,12 @@ const inherits = require("./surrogate.js");
 
 KidsBook.inherits(Book);
 
+// TODO: This code does not work if `showPicture` is defined in the KidsBook
+// file. Does this mean that it has to be defined _after_ inheritance?
+KidsBook.prototype.showPicture = function() {
+    console.log(`Picture shown from ${this.title}.`);
+};
+
 const mockingbird = new Book("To Kill a Mockingbird", "Harper Lee");
 const caterpillar = new KidsBook("The Very Hungry Caterpillar", "Eric Carle");
 
