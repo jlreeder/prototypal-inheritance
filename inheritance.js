7b737334b64ed42b9f2f7ce4f83cb7c5ae11f542
Jason Reeder <info@jasonreeder.com>
2017-02-26 12:02:49 -0800

    Extract inheritance function

--- inheritance.js
+++ inheritance.js
@@ -1,25 +1,11 @@
 const Book = require("./book.js");
-
-const mockingbird = new Book("To Kill a Mockingbird", "Harper Lee");
-
-Function.prototype.inherits = function(SuperClass) {
-  function Surrogate () {}
-  Surrogate.prototype = SuperClass.prototype;
-  this.prototype = new Surrogate();
-  this.prototype.constructor = this;
-};
-
 const KidsBook = require("./kidsBook.js");
+const inherits = require("./surrogate.js");
 
 KidsBook.inherits(Book);
 
-KidsBook.prototype.showPicture = function() {
-    console.log(`Picture shown from ${this.title}.`);
-};
-
-const caterpillar = new KidsBook("The Very Hungry Caterpillar",
-                                 "Eric Carle"
-);
+const mockingbird = new Book("To Kill a Mockingbird", "Harper Lee");
+const caterpillar = new KidsBook("The Very Hungry Caterpillar", "Eric Carle");
 
 mockingbird.turnPage();
 caterpillar.showPicture();
